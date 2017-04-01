6d7e0e117b965dd461f4575c5dd2e81a46e6dd35
Sarah Medeiros <sarahmedeiros@dhcp-wifi-8021x-155-41-99-112.bu.edu>
2017-04-01 19:55:00 -0400

    login interface

---
+++ User/app/src/main/java/com/sarahmedeiros/user/CreateNewUser.java
@@ -0,0 +1,38 @@
+package com.sarahmedeiros.user;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import com.sarahmedeiros.user.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CreateNewUser extends Fragment {
+
+    EditText edtEmail;
+    public CreateNewUser() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_create_new_user, container, false);
+        edtEmail = (EditText) v.findViewById(R.id.edtEmail);
+
+        return v;
+    }
+//    public void setEmailFromLogin(String email){
+////        edtEmail.setText(email);
+////    };
+
+
+}
---
+++ User/app/src/main/java/com/sarahmedeiros/user/Login.java
@@ -0,0 +1,81 @@
+package com.sarahmedeiros.user;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class Login extends Fragment {
+    Button btnSignIn;
+    Button btnRegister;
+    EditText edtPassword;
+    AutoCompleteTextView txtEmail;
+
+    public Login() {
+        // Required empty public constructor
+    }
+
+    public interface LoginListener{
+        public void goToNewUserFragment();
+        public void goToLoginFragment();
+//        public void sendEmail(String email);
+    }
+
+    LoginListener loginlistener;
+
+    //    //onAttach gets called when fragment attaches to Main Activity.  This is the right time to instantiate
+//    //our ControlFragmentListener, why?  Because we know the Main Activity was successfully created and hooked.
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        loginlistener = (LoginListener) context;  //context is a handle to the main activity, let's bind it to our interface.
+        //set number of stars
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_login, container, false);
+
+        btnSignIn = (Button) v.findViewById(R.id.btnSignIn);
+        btnRegister = (Button) v.findViewById(R.id.btnRegister);
+        txtEmail = (AutoCompleteTextView) v.findViewById(R.id.txtEmail);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//          // TODO: 3/31/17
+//         check email and password
+//                switch to logged on screen
+
+            }
+        });
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                loginlistener.sendEmail((txtEmail.getText().toString()));
+
+                loginlistener.goToNewUserFragment();
+            }
+        });
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                loginlistener.goToLoginFragment();
+            }
+        });
+        return v;
+    }
+
+
+}
