a4b31c377ef55a4d5cbc532fb794070bdc171adb
Ryan Walsh <walshmryan@gmail.com>
2017-04-06 15:47:28 -0400

    Added login fragment
    Not working yet, getting inflating errors

---
+++ GotYourBAC/src/main/java/com/google/example/tbmpskeleton/MainActivity.java
@@ -0,0 +1,68 @@
+package com.google.example.tbmpskeleton;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.undergrads.ryan.R;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.Fragment;
+
+public class MainActivity extends FragmentActivity implements LoginActivity.LoginListener {
+
+    //    Login loginFragment;
+    String loginTag = "login screen";
+    String createNewUser = "create new user";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+//        String loginTag = "login screen";
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        LoginActivity fragment = new LoginActivity();
+        fragmentTransaction.add(R.id.fragment_login, fragment, loginTag);
+        fragmentTransaction.commit();
+
+//        loginFragment = (Login) findFragmentById(R.layout.fragment_login);
+    }
+
+    @Override
+    public void goToNewUserFragment() {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+//        CreateNewUser newFragment = new CreateNewUser();
+//        String createNewUser = "create new user";
+//        transaction.replace(R.id.fragment_container, newFragment, createNewUser);
+        transaction.addToBackStack(null);
+
+        // Commit the transaction
+        transaction.commit();
+    }
+
+    public void goToLoginFragment() {
+//        check user login information
+//        then go to main activity page
+//        String loginTag = "login screen";
+//        String createNewUser = "create new user";
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        Fragment loginFragment = getSupportFragmentManager().findFragmentByTag(loginTag);
+        if (loginFragment!= null){
+            transaction.remove(loginFragment).commit();
+        }
+        Fragment newUserFragment = getSupportFragmentManager().findFragmentByTag(createNewUser);
+        if (newUserFragment != null){
+            transaction.remove(newUserFragment).commit();
+        }
+    }
+//
+//    @Override
+//    public void sendEmail(String e) {
+//        CreateNewUser receivingFragment = (CreateNewUser) getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+//        receivingFragment.setEmailFromLogin(e);
+//    }
+}
