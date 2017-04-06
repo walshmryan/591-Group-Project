87ca77de0338079a5b7dae1539154de33ea32756
Ryan Walsh <walshmryan@gmail.com>
2017-04-06 14:50:45 -0400

    Stroop test implementation with google games API
    Very crude approach but the game is working..somewhat

--- GotYourBAC/src/main/java/com/google/example/tbmpskeleton/SkeletonActivity.java
+++ GotYourBAC/src/main/java/com/google/example/tbmpskeleton/SkeletonActivity.java
@@ -23,6 +23,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -43,6 +44,9 @@ import com.google.example.games.basegameutils.BaseGameUtils;
 import com.undergrads.ryan.R;
 
 import java.util.ArrayList;
+import java.util.Random;
+
+import static android.view.View.VISIBLE;
 
 
 /**
@@ -53,12 +57,6 @@ import java.util.ArrayList;
  * automatch players; all known players play before automatch slots
  * are filled.
  *
- * INSTRUCTIONS: To run this sample, please set up
- * a project in the Developer Console. Then, place your app ID on
- * res/values/ids.xml. Also, change the package name to the package name you
- * used to create the client ID in Developer Console. Make sure you sign the
- * APK with the certificate whose fingerprint you entered in Developer Console
- * when creating your Client Id.
  *
  * @author Wolff (wolff@google.com), 2013
  */
@@ -106,6 +104,33 @@ public class SkeletonActivity extends Activity
     // taken an action on the match, such as takeTurn()
     public SkeletonTurn mTurnData;
 
+
+
+    Button btnStart;
+    Button btnYellow;
+    Button btnGreen;
+    Button btnRed;
+    Button btnBlue;
+    Button btnBlack;
+    TextView txtView0;
+    TextView txtView1;
+    TextView txtView2;
+    TextView txtView3;
+    TextView txtView4;
+    TextView comment;
+    int totalRight = 0;
+    int guesses = 0;
+    Random rand = new Random();
+    private Stopwatch stopwatch;
+    String[] colours = {"yellow", "green", "red", "blue", "black"};
+    Integer wordNumber = rand.nextInt(6);
+    Integer textNumber = rand.nextInt(6);
+    Integer colourNumber = rand.nextInt(6);
+    double time;
+    boolean started = false;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -124,6 +149,118 @@ public class SkeletonActivity extends Activity
 
         mDataView = ((TextView) findViewById(R.id.data_view));
         mTurnTextView = ((TextView) findViewById(R.id.turn_counter_view));
+
+        btnGreen = (Button) (findViewById(R.id.btnGreen));
+        btnRed = (Button) (findViewById(R.id.btnRed));
+        btnBlue = (Button) (findViewById(R.id.btnBlue));
+        btnStart = (Button) (findViewById(R.id.btnStart));
+        btnYellow = (Button) (findViewById(R.id.btnYellow));
+        btnBlack = (Button) (findViewById(R.id.btnBlack));
+        txtView0 = (TextView)(findViewById(R.id.txtView0));
+        txtView1 = (TextView)(findViewById(R.id.txtView1));
+        txtView2 = (TextView)(findViewById(R.id.txtView2));
+        txtView3 = (TextView)(findViewById(R.id.txtView3));
+        txtView4 = (TextView)(findViewById(R.id.txtView4));
+        comment = (TextView)(findViewById(R.id.comment));
+
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                enableButtons();
+                started = true;
+                guesses = 0;
+                totalRight = 0;
+                stopwatch = new Stopwatch();
+                comment.setVisibility(View.INVISIBLE);
+                txtView0.setVisibility(View.INVISIBLE);
+                txtView1.setVisibility(View.INVISIBLE);
+                txtView2.setVisibility(View.INVISIBLE);
+                txtView3.setVisibility(View.INVISIBLE);
+                txtView4.setVisibility(View.INVISIBLE);
+                nextWord();
+            }
+        });
+
+        btnYellow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                guesses += 1;
+                if (guesses <= 10) {
+                    checkCorrect(0);
+                } else {
+                    disableButtons();
+                    time = stopwatch.elapsedTime();
+                    comment.setText("You scored " + totalRight + "/10 in " + time + " seconds");
+                    comment.setVisibility(VISIBLE);
+                    mDataView.setText(Integer.toString(totalRight));
+                }
+            }
+        });
+
+        btnGreen.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                guesses += 1;
+                if (guesses <= 10) {
+                    checkCorrect(1);
+                } else {
+                    disableButtons();
+                    time = stopwatch.elapsedTime();
+                    comment.setText("You scored " + totalRight + "/10 in " + time + " seconds");
+                    comment.setVisibility(VISIBLE);
+                    mDataView.setText(Integer.toString(totalRight));
+                }
+            }
+        }));
+
+        btnRed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                guesses += 1;
+                if (guesses <= 10) {
+                    checkCorrect(2);
+                } else {
+                    disableButtons();
+                    time = stopwatch.elapsedTime();
+                    comment.setText("You scored " + totalRight + "/10 in " + time + " seconds");
+                    comment.setVisibility(VISIBLE);
+                    mDataView.setText(Integer.toString(totalRight));
+                }
+            }
+        });
+
+        btnBlue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                guesses += 1;
+                if (guesses <= 10) {
+                    checkCorrect(3);
+                } else {
+                    disableButtons();
+                    time = stopwatch.elapsedTime();
+                    comment.setText("You scored " + totalRight + "/10 in " + time + " seconds");
+                    comment.setVisibility(VISIBLE);
+                    mDataView.setText(Integer.toString(totalRight));
+                }
+            }
+        });
+
+        btnBlack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                guesses += 1;
+                if (guesses <= 10) {
+                    checkCorrect(4);
+                } else {
+                    disableButtons();
+                    time = stopwatch.elapsedTime();
+                    comment.setText("You scored " + totalRight + "/10 in " + time + " seconds");
+                    comment.setVisibility(VISIBLE);
+                    mDataView.setText(totalRight);
+                }
+            }
+        });
     }
 
     @Override
@@ -299,7 +436,9 @@ public class SkeletonActivity extends Activity
         String nextParticipantId = getNextParticipantId();
         // Create the next turn
         mTurnData.turnCounter += 1;
-        mTurnData.data = mDataView.getText().toString();
+
+        mTurnData.data = totalRight + "";
+        //mTurnData.data = mDataView.getText().toString();
 
         showSpinner();
 
@@ -768,12 +907,6 @@ public class SkeletonActivity extends Activity
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.sign_in_button:
-                // Check to see the developer who's running this sample code read the instructions :-)
-                // NOTE: this check is here only because this is a sample! Don't include this
-                // check in your actual production app.
-                if (!BaseGameUtils.verifySampleSetup(this, R.string.app_id)) {
-                    Log.w(TAG, "*** Warning: setup problems detected. Sign in may not work!");
-                }
 
                 mSignInClicked = true;
                 mTurnBasedMatch = null;
@@ -789,5 +922,90 @@ public class SkeletonActivity extends Activity
                 setViewVisibility();
                 break;
         }
+    }
+
+    private void textSet(TextView txtView, int wordNum, int colourNum){
+        txtView.setText(colours[wordNumber]);
+        if (colourNum == 0) {
+            txtView.setTextColor(getResources().getColor(R.color.yellow));
+            txtView.setVisibility(VISIBLE);
+        }
+        else if (colourNum == 1) {
+            txtView.setTextColor(getResources().getColor(R.color.green));
+            txtView.setVisibility(VISIBLE);
+        }
+        else if (colourNum == 2) {
+            txtView.setTextColor(getResources().getColor(R.color.red));
+            txtView.setVisibility(VISIBLE);
+        }
+        else if (colourNum == 3) {
+            txtView.setTextColor(getResources().getColor(R.color.blue));
+            txtView.setVisibility(VISIBLE);
+        }
+        else if (colourNum == 4) {
+            txtView.setTextColor(getResources().getColor(R.color.black));
+            txtView.setVisibility(VISIBLE);
+        }
+    }
+
+    private void disableButtons(){
+        btnYellow.setEnabled(false);
+        btnGreen.setEnabled(false);
+        btnBlack.setEnabled(false);
+        btnBlue.setEnabled(false);
+        btnRed.setEnabled(false);
+    }
+
+    private void enableButtons(){
+        btnYellow.setEnabled(true);
+        btnGreen.setEnabled(true);
+        btnBlack.setEnabled(true);
+        btnBlue.setEnabled(true);
+        btnRed.setEnabled(true);
+    }
+
+    private void nextWord() {
+        wordNumber = rand.nextInt(5);
+        textNumber = rand.nextInt(5);
+        colourNumber = rand.nextInt(5);
+
+        // TESTING
+        //wordNumber = 3;
+        //textNumber = 4;
+        //colourNumber = 1;
+        //TESTING
+
+        if (textNumber == 0) {
+            textSet(txtView0, wordNumber, colourNumber);
+        }
+        else if (textNumber == 1) {
+            textSet(txtView1, wordNumber, colourNumber);
+        }
+        else if (textNumber == 2) {
+            textSet(txtView2, wordNumber, colourNumber);
+        }
+        else if (textNumber == 3) {
+            textSet(txtView3, wordNumber, colourNumber);
+        }
+        else if (textNumber == 4) {
+            textSet(txtView4, wordNumber, colourNumber);
+        }
+    }
+
+    private boolean checkCorrect(int c) {
+        if (colourNumber == c) {
+            //txtView0.setText("TEST");
+            //txtView0.setVisibility(VISIBLE);
+            totalRight += 1;
+            txtView0.setVisibility(View.INVISIBLE);
+            txtView1.setVisibility(View.INVISIBLE);
+            txtView2.setVisibility(View.INVISIBLE);
+            txtView3.setVisibility(View.INVISIBLE);
+            txtView4.setVisibility(View.INVISIBLE);
+            nextWord();
+            return true;
+        }
+
+        return false;
     }
 }
