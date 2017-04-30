02a6328d04272c9b6fdfe60795e5a568d41e88f2
Sarah Medeiros <sarahmedeiros@dhcp-wifi-8021x-155-41-53-249.bu.edu>
2017-04-29 23:02:09 -0400

    sams game

---
+++ GotYourBAC/src/main/java/com/undergrads/ryan/fragment_tilt_home.java
@@ -0,0 +1,71 @@
+package com.undergrads.ryan;
+
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.io.Serializable;
+
+/**
+ * Created by Samantha on 4/23/2017.
+ */
+
+public class fragment_tilt_home extends Fragment{
+    Button btnPlay;
+    Button btnLearn;
+
+    OnTiltHomeListener mCallback;
+
+    public void onSaveInstanceState(final Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+
+    // Container Activity must implement this interface
+    public interface OnTiltHomeListener  {
+        void onPlaySelect(boolean selected);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        // This makes sure that the container activity has implemented
+        // the callback interface. If not, it throws an exception
+        try {
+            mCallback = (OnTiltHomeListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnTiltHomeListener");
+        }
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_tilt_home, container, false);
+        btnPlay = (Button) v.findViewById(R.id.btnPlay);
+        btnLearn = (Button) v.findViewById(R.id.btnLearn);
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCallback.onPlaySelect(true);
+            }
+        });
+
+        btnLearn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCallback.onPlaySelect(false);
+            }
+        });
+
+        return  v;
+
+    }
+
+}
---
+++ GotYourBAC/src/main/java/com/undergrads/ryan/tilt_learn.java
@@ -0,0 +1,22 @@
+package com.undergrads.ryan;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class tilt_learn extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_tilt_learn, container, false);
+
+
+        return v;
+    }
+}
