371dcc516a7197c0f5282e7d8f6517a1674ebf7f
Sarah Medeiros <sarahpmedeiros@gmail.com>
2017-04-30 20:56:59 -0400

    readdded file

---
+++ GotYourBAC/src/main/java/com/undergrads/ryan/CreateNewUser.java
@@ -0,0 +1,87 @@
+package com.undergrads.ryan;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+//import com.google.example.tbmpskeleton.R;
+
+//import com.undergrads.ryan.R;
+
+/**
+ * Fragment class to create a new user
+ */
+public class CreateNewUser extends Fragment {
+
+    EditText edtEmail;
+    EditText password;
+    EditText firstName;
+    EditText lastName;
+    EditText weight;
+    Spinner gender;
+    String strGender;
+    Button createUser;
+
+    public CreateNewUser() {
+        // Required empty public constructor
+    }
+
+    public interface newUserListener{
+        public void goToCreateNew(String email,
+              String password,String firstName,
+              String lastName, String weight,String gender);
+
+    }
+
+    newUserListener userListener;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_create_new_user, container, false);
+//        initialize buttons etc
+        edtEmail = (EditText) v.findViewById(R.id.edtEmail);
+        createUser = (Button)v.findViewById(R.id.btnCreateUser);
+        password = (EditText)v.findViewById(R.id.edtPassword);
+        firstName = (EditText)v.findViewById(R.id.edtFirstName);
+        lastName =(EditText)v.findViewById(R.id.edtLastName);
+        weight = (EditText)v.findViewById(R.id.edtWeight);
+        gender = (Spinner)v.findViewById(R.id.sexSpinner);
+
+//        on create user add the user to the database
+//        continue registration process
+        createUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                strGender = gender.getSelectedItem().toString();
+                userListener.goToCreateNew(edtEmail.getText().toString(),password.getText().toString(),firstName.getText().toString(),lastName.getText().toString(),
+                        weight.getText().toString(),strGender);
+            }
+        });
+        return v;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        // This makes sure that the container activity has implemented
+        // the callback interface. If not, it throws an exception
+        try {
+            userListener = (CreateNewUser.newUserListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnHeadlineSelectedListener");
+        }
+    }
+
+
+}
